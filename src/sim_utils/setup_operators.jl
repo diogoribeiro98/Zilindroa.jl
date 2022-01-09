0b0b14fa035e217e5444984ee7359a780113af2c
dribeiro <diogo.gnc.ribeiro@gmail.com>
2022-01-09 15:29:10 +0000

    Update differential operator setup routine (notation)

--- src/sim_utils/setup_operators.jl
+++ src/sim_utils/setup_operators.jl
@@ -15,11 +15,11 @@ function set_up_operator(p::Param)
     theta = [ (i-1)*dth for i in 1:thnodes]
 
     #Create Differential Operators 
-    Dr  =   Diff_Operator_2D(1 ,1, [dx,dy] , [Nx,Ny]) 
-    Drr =   Diff_Operator_2D(2 ,1, [dx,dy] , [Nx,Ny])
+    Dr  =   Diff_Operator_2D(1 ,1, [dr,dth] , [rnodes,thnodes]) 
+    Drr =   Diff_Operator_2D(2 ,1, [dr,dth] , [rnodes,thnodes])
    
-    Dp  =  Diff_Operator_2D_Periodic(1 ,2, [dx,dy] , [Nx,Ny]) 
-    Dpp =  Diff_Operator_2D_Periodic(2 ,2, [dx,dy] , [Nx,Ny])
+    Dp  =  Diff_Operator_2D_Periodic(1 ,2, [dr,dth] , [rnodes,thnodes]) 
+    Dpp =  Diff_Operator_2D_Periodic(2 ,2, [dr,dth] , [rnodes,thnodes])
     
     one_over_r2 = spzeros(rnodes*thnodes,rnodes*thnodes)
 
@@ -29,8 +29,8 @@ function set_up_operator(p::Param)
 
     RHS_Opp = sparse(Drr + (0.25)*one_over_r2*I  + one_over_r2*Dpp);
 
-    av1 = zeros(Nx*Ny)
-    av2 = zeros(Nx*Ny)
+    av1 = zeros(rnodes*thnodes)
+    av2 = zeros(rnodes*thnodes)
     
     #Operator structure
     return Opps(r, theta , av1 , av2  , Dr, Dp, RHS_Opp)
